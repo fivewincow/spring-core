cac87b60b880facb96deeeaa6d0fd75f487bb903
fivewincow <fivewincow@gmail.com>
2022-04-04 12:54:02 +0900

    bean scope

---
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,34 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Locale;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) throws InterruptedException{
+        String requestURL = request.getRequestURI().toString();
+        myLogger.setRequestURL(requestURL);
+
+        System.out.println("myLogger.getClass() = " + myLogger.getClass());
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "OK";
+
+    }
+}
